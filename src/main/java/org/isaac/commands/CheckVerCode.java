d83c3104a2b8fe355020cce56c7787d0f737a7d1
O-Isaac <soporte@proyectograndorder.es>
2024-11-22 20:23:14 +0100

    feat(CheckVerCode): Add support for extracting global-metadata.dat from APK/XAPK files

--- src/main/java/org/isaac/commands/CheckVerCode.java
+++ src/main/java/org/isaac/commands/CheckVerCode.java
@@ -1,16 +1,17 @@
 package org.isaac.commands;
 
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.isaac.entities.AppEntity;
 import org.isaac.utils.MyLogger;
 import org.isaac.utils.StringLiteralExtractor;
 
+import org.isaac.utils.ZipExtractor;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 
 import java.io.File;
+import java.nio.file.Path;
 import java.util.concurrent.Callable;
 
 @Command(
@@ -20,8 +21,8 @@ import java.util.concurrent.Callable;
         description = "Compare two stringliteral.json and return updated verCode"
 )
 public class CheckVerCode implements Callable<Integer> {
-    @Option(names = { "-g", "--global-metadata" }, description = "Path for file global-metadata.dat")
-    String globalMetadataPath;
+    @Option(names = { "-xa", "--apk"}, description = "Path for xapk.")
+    String xapkPath;
 
     @Option(names = { "-av", "--appversion" }, description = "What version in this verCode.")
     String appVer;
@@ -29,24 +30,32 @@ public class CheckVerCode implements Callable<Integer> {
     @Option(names = { "-o", "--output"}, description = "Specify the output where is the file. (include name & ext)")
     String output;
 
-    private static final MyLogger logger = new MyLogger(CheckVerCode.class);
+    private static final MyLogger LOGGER = new MyLogger(CheckVerCode.class);
 
     @Override
     public Integer call() throws Exception {
-        if (globalMetadataPath == null || appVer == null || output == null) return 1;
+        if (xapkPath == null || appVer == null || output == null) return 1;
 
         ObjectMapper mapper = new ObjectMapper();
 
-        logger.info("Getting global-metadata.dat " + globalMetadataPath);
-        StringLiteralExtractor literalExtractor = new StringLiteralExtractor(globalMetadataPath).extract();
-        
-        logger.info("Getting VerCode...");
+        LOGGER.info("Getting global-metadata.dat from apk " + xapkPath);
+        ZipExtractor xapkExtractor = new ZipExtractor(xapkPath);
+        Path apkPath = xapkExtractor.extract(
+            "com.aniplex.fategrandorder.apk",
+            ""
+        );
+
+        ZipExtractor globalMetadataExtrator = new ZipExtractor(apkPath.toString());
+        Path globalMetadaPath = globalMetadataExtrator.extract(
+            "assets/bin/Data/Managed/Metadata/global-metadata.dat",
+            ""
+        );
+
+        StringLiteralExtractor literalExtractor = new StringLiteralExtractor(globalMetadaPath.toString()).extract();
         AppEntity appEntity = new AppEntity(literalExtractor.getVerCodeString(), appVer);
 
-        logger.info("Writing verCode (" + appEntity.getVerCode() + ") of version (" + appEntity.getAppVer() + ")");
         mapper.writeValue(new File(output), appEntity);
-
-        logger.info("Done!");
+        LOGGER.info("Writed file on " + output + " completed!");
         return 0;
     }
 }
