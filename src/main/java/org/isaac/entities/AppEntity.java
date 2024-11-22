56bc3f575da20a92780663c0c6180835027a0de1
O-Isaac <soporte@proyectograndorder.es>
2024-11-22 20:22:11 +0100

    refactor(AppEntity): Add logger for AppEntity Initialization

--- src/main/java/org/isaac/entities/AppEntity.java
+++ src/main/java/org/isaac/entities/AppEntity.java
@@ -1,12 +1,18 @@
 package org.isaac.entities;
 
+import org.isaac.commands.CheckVerCode;
+import org.isaac.utils.MyLogger;
+
 public class AppEntity {
     String verCode;
     String appVer;
 
+    private final MyLogger LOGGER = new MyLogger(AppEntity.class);
+
     public AppEntity(String verCode, String appVer) {
         this.verCode = verCode;
         this.appVer = appVer;
+        this.LOGGER.info("AppVer=" + appVer + ", VerCode=" + verCode);
     }
 
     public String getAppVer() {
