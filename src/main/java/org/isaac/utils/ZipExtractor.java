112af84061f10b02b1767f75342b6b25bdb2c5ec
O-Isaac <soporte@proyectograndorder.es>
2025-04-01 11:40:54 +0200

    feat(ZipExtractor): Update extract method to handle multiple files and return optional path

--- src/main/java/org/isaac/utils/ZipExtractor.java
+++ src/main/java/org/isaac/utils/ZipExtractor.java
@@ -1,43 +1,42 @@
 package org.isaac.utils;
 
-import org.isaac.commands.CheckVerCode;
-
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.util.List;
+import java.util.Optional;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
 
 public class ZipExtractor {
-    private String filePath;
+    private final String filePath;
     private static final MyLogger LOG = new MyLogger(ZipExtractor.class);
 
     public ZipExtractor(String filePath) {
         this.filePath = filePath;
     }
-
-    public Path extract(String fileName, String pathDest) throws IOException {
-        Path extractPath = null;
-
+    
+    public Optional<Path> extract(List<String> files, String dest) throws IOException {
         try (ZipInputStream zip = new ZipInputStream(new FileInputStream(this.filePath))) {
-            ZipEntry entry = zip.getNextEntry();
+            ZipEntry entry;
+            while ((entry = zip.getNextEntry()) != null) {
+                String entryName = entry.getName();
+                
+                if (files.contains(entryName)) {
+                    LOG.info("Extracting " + entryName + " (" + entry.getCrc() + ")");
+                    
+                    Path pathEntryFile = Paths.get(entryName).getFileName();
+                    Path destinationFolder = Paths.get(dest).resolve(pathEntryFile);
 
-            while (entry != null) {
-                if (entry.getName().equals(fileName)) {
-                    LOG.info("Extracting " + entry.getName() + " (" + entry.getCrc() + ")");
-                    String[] fileNameParts = fileName.splitWithDelimiters("/", 0);
-                    Path filePathDest = Paths.get(pathDest + fileNameParts[fileNameParts.length - 1]);
-                    extractPath = filePathDest;
-                    Files.copy(zip,filePathDest, StandardCopyOption.REPLACE_EXISTING);
+                    Files.copy(zip, destinationFolder.getFileName(), StandardCopyOption.REPLACE_EXISTING);
+                    return Optional.of(destinationFolder);
                 }
-
-                entry = zip.getNextEntry();
             }
         }
 
-        return extractPath;
+        return Optional.empty();
     }
 }
